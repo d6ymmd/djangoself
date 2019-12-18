7f9574b46c6c78aef6cac80147d94b4e1a29b6aa
d6ymmd <d6ymmd@gmail.com>
2019-12-18 11:18:21 +0900

    index에서 latest_question_list를 보여줌

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index")
+    latest_question_list = Question.object.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
