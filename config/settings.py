540a7c6001b04bb0a4b54e7e900cdbad607bdba3
d6ymmd <d6ymmd@gmail.com>
2019-12-18 09:36:27 +0900

    LANGUAGE_CODE및TIME_ZONE설정

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+from django.utils.translation import gettext_lazy as _
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -103,9 +105,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE = [
+    ('ko', _('Korean')),
+]
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ko'
+
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
