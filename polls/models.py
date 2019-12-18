5239a9f22c51ec92236743e63c96ac0b543304cf
d6ymmd <d6ymmd@gmail.com>
2019-12-18 09:55:59 +0900

    Question, Choice의 __str__메서드 정의

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('발행날짜')
+
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return sef.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -11,3 +18,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.choice_text
